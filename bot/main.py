ed873549ce55aa5464f5aeb057ea7950a80330fc
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-08-17 19:00:45 +0300

    Add good to buttons

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,12 @@
 import logging
 import os
 import json
+import pprint
 
 from functools import partial
 
 import redis
+import more_itertools
 
 from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
@@ -17,14 +19,19 @@ from telegram.ext import (
     CallbackQueryHandler
 )
 
+from motlin_api import get_shop_products, get_access_token
+
+
 logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext):
-    keyboard = [[InlineKeyboardButton("Option 1", callback_data="option_1"),
-                 InlineKeyboardButton("Option 2", callback_data="option_2")],
-                [InlineKeyboardButton("Option 3", callback_data="option_3")]
-                ]
+def start(update: Update, context: CallbackContext, motlin_access_token: str):
+    goods = get_shop_products(motlin_access_token)["data"]
+    prepared_keyboard = [
+        InlineKeyboardButton(item["name"], callback_data=item["id"])
+        for item in goods
+    ]
+    keyboard = list(more_itertools.chunked(prepared_keyboard, 2))
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
@@ -47,7 +54,8 @@ def echo(update: Update, context: CallbackContext):
     return "ECHO"
 
 
-def handle_messages(update: Update, context: CallbackContext, database):
+def handle_messages(update: Update, context: CallbackContext,
+                    database: redis, motlin_access_token: str):
     if update.message:
         user_reply = update.message.text
         chat_id = str(update.message.chat_id)
@@ -64,7 +72,7 @@ def handle_messages(update: Update, context: CallbackContext, database):
         user_state = users[chat_id]
 
     states_functions = {
-        "START": start,
+        "START": partial(start, motlin_access_token=motlin_access_token),
         "ECHO": echo,
         "USER_CHOICE": button
     }
@@ -89,7 +97,10 @@ def main():
     redis_host = os.environ["REDIS_HOST"]
     redis_port = os.environ["REDIS_PORT"]
     redis_password = os.environ["REDIS_PASSWORD"]
-    updater = Updater(telegram_bot_token)
+    motlin_client_id = os.environ["MOTLIN_CLIENT_ID"]
+    motlin_access_token = get_access_token(
+        motlin_client_id=motlin_client_id
+    )
 
     database = redis.Redis(
         host=redis_host,
@@ -101,22 +112,35 @@ def main():
         setup_redis = json.dumps(users)
         database.set("users", setup_redis)
 
+    updater = Updater(telegram_bot_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(
         CallbackQueryHandler(
-            partial(handle_messages, database=database)
+            partial(
+                handle_messages,
+                database=database,
+                motlin_access_token=motlin_access_token
+            )
         )
     )
     dispatcher.add_handler(
         MessageHandler(
             Filters.text,
-            partial(handle_messages, database=database)
+            partial(
+                handle_messages,
+                database=database,
+                motlin_access_token=motlin_access_token
+            )
         )
     )
     dispatcher.add_handler(
         CommandHandler(
             'start',
-            partial(handle_messages, database=database)
+            partial(
+                handle_messages,
+                database=database,
+                motlin_access_token=motlin_access_token
+            )
         )
     )
 
