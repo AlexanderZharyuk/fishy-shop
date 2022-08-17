61f2768bc48959925387431f0377ba3da5e70ce8
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-08-17 17:03:01 +0300

    Add cart operations

--- bot/motlin_api.py
+++ bot/motlin_api.py
@@ -8,12 +8,12 @@ from dotenv import load_dotenv
 
 def get_access_token(motlin_client_id: str) -> str:
     url = "https://api.moltin.com/oauth/access_token"
-    data = {
+    request_data = {
         'client_id': motlin_client_id,
         'grant_type': 'implicit',
     }
 
-    response = requests.post(url=url, data=data)
+    response = requests.post(url=url, data=request_data)
     response.raise_for_status()
 
     api_response = response.json()
@@ -35,11 +35,66 @@ def get_shop_products(api_access_token: str) -> dict:
     return api_response
 
 
+def get_user_cart(user_id: str, api_access_token: str) -> dict:
+    url = f"https://api.moltin.com/v2/carts/{user_id}"
+    headers = {
+        "Authorization": f"Bearer {api_access_token}"
+    }
+
+    response = requests.get(url=url, headers=headers)
+    response.raise_for_status()
+
+    api_response = response.json()
+    return api_response
+
+
+def add_item_to_cart(user_id: str, api_access_token: str, item: dict) -> None:
+    url = f"https://api.moltin.com/v2/carts/{user_id}/items"
+    headers = {
+        "Authorization": f"Bearer {api_access_token}",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "data": {
+            "type": "custom_item",
+            "name": item["name"],
+            "sku": item["sku"],
+            "description": item["description"],
+            "quantity": 1,
+            "price": item["price"][0]
+        }
+    }
+
+    response = requests.post(url=url, headers=headers, json=payload)
+    response.raise_for_status()
+
+
+def get_items_from_cart(user_id: str, api_access_token: str) -> dict:
+    url = f"https://api.moltin.com/v2/carts/{user_id}/items"
+    headers = {
+        "Authorization": f"Bearer {api_access_token}",
+    }
+
+    response = requests.get(url=url, headers=headers)
+    response.raise_for_status()
+
+    api_response = response.json()
+    return api_response
+
+
 def main():
     load_dotenv()
     motlin_client_id = os.environ["MOTLIN_CLIENT_ID"]
     motlin_access_token = get_access_token(motlin_client_id)
-    get_shop_products(motlin_access_token)
+    all_products = get_shop_products(motlin_access_token)
+    user_choice = all_products["data"][1]
+    user_id = "abc"
+    add_item_to_cart(
+        user_id=user_id,
+        api_access_token=motlin_access_token,
+        item=user_choice
+    )
+    get_items_from_cart(user_id=user_id, api_access_token=motlin_access_token)
 
 
 if __name__ == "__main__":
